16a69bd01b0102429bdec1af1f33222aa59970de
DIPANSHU DHAGE <156861498+dipanshudhage@users.noreply.github.com>
2026-03-25 22:56:57 +0530

    Update API base URL to use environment variable

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // 🔥 BASE API
-const API_BASE = "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_API_URL;;
 
 // 🔥 CREATE AXIOS INSTANCE
 const api = axios.create({
